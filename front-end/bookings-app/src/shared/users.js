be326480069e99e8d7762c533c40330d0d709bfa
Samyak <samyjain@adobe.com>
2020-09-11 17:35:01 +0530

    Added basic structure for data
    check shared folder in src.

---
+++ front-end/bookings-app/src/shared/users.js
@@ -0,0 +1,10 @@
+export const users = [
+    {
+        id: 0,
+        username: '',
+        personal_det: {
+                    //name,mobile,email, bla bal bal    
+            },
+        booking_history: [] //array of booking_ids he has done
+    }
+]
