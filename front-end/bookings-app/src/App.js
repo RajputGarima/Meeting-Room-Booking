16981fba1d0ad54278eaf25fad3b326fff571217
Samyak <samyjain@adobe.com>
2020-09-11 15:40:06 +0530

    Added layoutlist component
    created shared folder in src to store the mock data

--- front-end/bookings-app/src/App.js
+++ front-end/bookings-app/src/App.js
@@ -6,6 +6,7 @@ import {Switch, Route, Redirect, BrowserRouter} from 'react-router-dom';
 import Menu from './components/Menu';
 import AddRoom from './components/AddRoom'
 import AddClient from './components/AddClientDetails'
+import LayoutsList from './components/LayoutsList';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path='/menu' component={() => <Menu/> } /> 
             <Route path='/addroom' component={() => <AddRoom/> } /> 
             <Route path='/addclient' component={()=> <AddClient/>} />
+            <Route path='/layoutslist' component={()=> <LayoutsList/>} />
             <Redirect to = '/menu' />
         </Switch>
     </BrowserRouter>
