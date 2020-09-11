f30d8f9e8d962b242d563345ca353c0a696825c0
Shripad Deshmukh <deshmukh.shripad.v@gmail.com>
2020-09-11 16:24:49 +0530

    Added latest bookings, split large components

--- front-end/bookings-app/src/components/Dashboard/index.js
+++ front-end/bookings-app/src/components/Dashboard/index.js
@@ -5,15 +5,17 @@ import {
     Route,
     Link
   } from "react-router-dom";
-import { Tab, Segment, Header, Grid, Container, Divider, Statistic, Button, Image, List } from 'semantic-ui-react';
+import { Tab, Segment, Header, Grid, Container, Divider, Statistic, Button, Item, Image, List, Placeholder } from 'semantic-ui-react';
 
 // Calendar date picking
-import DatePicker from 'react-date-picker'
+import DatePicker from 'react-date-picker';
+
+// Scroll Bar
 
 const BookingsCounter = () =>{
   return(
     <Segment textAlign='center'>
-      <Grid columns='equal' padded divided>
+      <Grid columns='equal' divided>
         <Grid.Row columns='equal' stretched >
           <Grid.Column>
             <Statistic>
@@ -40,55 +42,117 @@ const BookingsCounter = () =>{
   )
 }
 
+
+
+const LatestBookings = ()=>{
+  const [bookings, setBookings] = 
+    useState([{room:"Small Conference Room", bookedForDate:new Date(2020, 3, 25), bookedForDuration:"8-9", bookedOnDate:new Date('March 25, 2020 09:24:00'), user:"Mallory Norman"}, 
+              {room:"Large Conference Room", bookedForDate:new Date(2020, 3, 17), bookedForDuration:"Morning", bookedOnDate:new Date('February 17, 2020 08:24:00'), user:"Dorothy Jacobs"}, 
+              {room:"Panoramic Room", bookedForDate:new Date(2020, 3, 21), bookedForDuration:"Afternoon", bookedOnDate:new Date('January 17, 2020 01:24:00'), user:"Jason McCarty"}])
+
+  bookings.sort((booking1, booking2)=> booking2.bookedOnDate - booking1.bookedOnDate)
+  // Top three latest Bookings
+  const latestBookings = [];
+  for (let i=0; i < 3 && i < bookings.length; i++){
+    latestBookings.push(bookings[i]);
+  }
+  console.log(latestBookings)
+  return (
+    <Grid.Column>
+      <Segment style={{height: '40vh'}}>
+        <Header as='h3'>
+          Latest Bookings
+        </Header>
+        <Divider/>
+        <List as='ul'>
+          {latestBookings.map(booking => (
+            <List.Item as='li'>
+              <b>{booking.room}</b><br/>
+              {booking.bookedForDate.toLocaleDateString()}<br/>
+              <i>{booking.user}</i>
+            </List.Item>)
+            )
+          }
+        </List>
+    
+      </Segment>
+    </Grid.Column>
+  );
+}
+
+
+const Reservations = ()=>{
+  const [date, setDate] = useState();
+  <GetReservations date/>
+  const getReservations = null;
+  return (
+    <Grid.Column>
+      <Segment style={{height: '40vh'}}>
+        <Header as='h3'>
+          Reservations
+        </Header>
+        <Divider/>
+        
+        <DatePicker 
+          value={date} 
+          onChange={setDate} 
+          dayPlaceholder="dd"
+          monthPlaceholder="mm"
+          yearPlaceholder="yyyy"
+        />
+        
+        <Button content='Print' icon='print' floated='right' size='tiny' color='blue'/>
+
+        <Segment style={{ overflow: 'auto', maxHeight:'30vh' }}>
+          <List as='ul'>
+            {/* {bookingForDate.map(booking => (<List.Item as='li'>
+                                              <b>{booking.room}</b><br/>
+                                              {booking.bookedForDate.toLocaleDateString()}<br/>
+                                              <i>{booking.user}</i>
+                                            </List.Item>))} */}
+          </List>
+        </Segment>
+        
+      </Segment>
+    </Grid.Column>
+  )
+
+}
+
+const QuickLinks = () => (
+  <Grid.Column>
+    <Segment style={{height: '40vh'}}>
+      <Header as='h3'>
+        Quick Links
+      </Header>
+      <Divider/>
+      <Router>
+        <List>
+          <List.Item as={Link} to={'/'}>+ Add booking</List.Item>
+          <List.Item as={Link} to={'/addroom'}>+ Add room</List.Item>
+        </List>
+
+        <List>
+          <List.Item as={Link} to='/'>View Bookings</List.Item>
+          <List.Item as={Link} to='/'>View Rooms</List.Item>
+          <List.Item as={Link} to='/'>Manage Equipment</List.Item>
+          <List.Item as={Link} to='/'>Edit Booking form</List.Item>
+          <List.Item as={Link} to='/'>Language settings</List.Item>
+          <List.Item as={Link} to='/'>Back up your files</List.Item>
+        </List>
+      </Router>
+    </Segment>
+  </Grid.Column>
+)
 const DashboardInternal = () =>{
 
-  const [date, setDate] = useState(new Date())
   return(
     <Container textAlign='left'>
-    <Grid columns='equal' padded divided>
+    <Grid divided padded>
       <Grid.Row columns='equal' >
-        <Grid.Column>
-          <Segment>
-            <Header as='h4'>
-              Latest Bookings
-            </Header>
-            <Divider/>
-          </Segment>
-        </Grid.Column>
-
-        <Grid.Column>
-          <Segment>
-            <Header as='h4'>
-              Reservations
-            </Header>
-            <Divider/>
-            <DatePicker value={date} onChange={setDate}/><Button icon='print'/>
-          </Segment>
-        </Grid.Column>
-        
-        <Grid.Column>
-          <Segment>
-            <Header as='h4'>
-              Quick Links
-            </Header>
-            <Divider/>
-            <Router>
-              <List>
-                <List.Item as={Link} to='/'>+ Add booking</List.Item>
-                <List.Item as={Link} to='/'>+ Add room</List.Item>
-              </List>
-
-              <List>
-                <List.Item as={Link} to='/'>View Bookings</List.Item>
-                <List.Item as={Link} to='/'>View Rooms</List.Item>
-                <List.Item as={Link} to='/'>Manage Equipment</List.Item>
-                <List.Item as={Link} to='/'>Edit Booking form</List.Item>
-                <List.Item as={Link} to='/'>Language settings</List.Item>
-                <List.Item as={Link} to='/'>Back up your files</List.Item>
-              </List>
-            </Router>
-          </Segment>
-        </Grid.Column>
+        <LatestBookings/>
+        <Reservations/>
+        <QuickLinks/>
       </Grid.Row>
     </Grid>
     </Container>
@@ -96,7 +160,7 @@ const DashboardInternal = () =>{
 }
 
 
-const TimeDateStats = () =>  {
+const LoginStats = () =>  {
   const [currentTime, setCurrentTime] = useState((new Date()).toLocaleTimeString());
   const [currentDate, setCurrentDate] = useState((new Date()).toLocaleDateString());
 
@@ -113,11 +177,11 @@ const TimeDateStats = () =>  {
   return (
     <Segment inverted vertical>
       <Container>
-          <Grid columns={2} padded>
+          <Grid columns={2}>
             <Grid.Column floated='left' textAlign='left'>
               Last Logged In
             </Grid.Column>
-            <Grid.Column floated='right' textAlign='right' padded>
+            <Grid.Column floated='right' textAlign='right'>
               {currentTime}<br/>
               <small>{currentDate}</small>
             </Grid.Column>
@@ -138,7 +202,7 @@ return (
         </Segment>
         <BookingsCounter/>
         <DashboardInternal/>
-        <TimeDateStats/>
+        <LoginStats/>
     </Tab.Pane> 
     </div>
 )
